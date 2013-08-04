9233775470ce360fd6edabea7840ae9e487f2a5e
Michael Elsdörfer <michael@elsdoerfer.com>
2013-08-04 14:49:59 +0200

    I'm claiming authership on this.

--- eiscp/__init__.py
+++ eiscp/__init__.py
@@ -1,6 +1,6 @@
 """Commands and control for the Onkyo eISCP interface.
 """
 
-__author__ = 'Will Nowak <wan@ccs.neu.edu>'
+__author__ = 'Michael Elsdoerfer <michael@elsdoerfer.com>'
 
 from core import eISCP
