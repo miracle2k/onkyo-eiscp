caf2bf61ab763baf66fca2dd217139d9b13e0881
Michael Elsdörfer <michael@elsdoerfer.com>
2017-07-15 00:54:31 +0100

    Prepare a new release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ f.close()
 
 setup(
     name='onkyo-eiscp',
-    version='1.2.1',
+    version='1.2.2',
     url='https://github.com/miracle2k/onkyo-eiscp',
     license='MIT',
     author='Michael Elsdörfer',
