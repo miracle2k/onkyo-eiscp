17b1751e0682be2261941f88ed28e92479b1d1a5
Michael Elsdörfer <michael@elsdoerfer.com>
2017-08-17 23:16:09 +0100

    Commit forgotten release changes for 1.2.3.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ f.close()
 
 setup(
     name='onkyo-eiscp',
-    version='1.2.2',
+    version='1.2.3',
     url='https://github.com/miracle2k/onkyo-eiscp',
     license='MIT',
     author='Michael Elsdörfer',
