cc1e14bb2b383a6b7df61931984067aec27bb491
Daniel Fairhurst <daniel.fairhurst.82@gmail.com>
2024-03-09 00:47:04 -0500

    Release 1.2.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ f.close()
 
 setup(
     name='dannytrigo-onkyo-eiscp',
-    version='1.2.9',
+    version='1.2.10',
     url='https://github.com/dannytrigo/onkyo-eiscp',
     license='MIT',
     author='Michael Elsdörfer',
